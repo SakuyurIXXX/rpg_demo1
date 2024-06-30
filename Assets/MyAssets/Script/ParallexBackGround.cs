5b4653d1a3368b6fcdb8e7b1113ef7e56cb64c13
horikawa <horikawaxxx0325@gmail.com>
2024-06-30 16:38:47 +0800

    增加背景无限延申效果

--- Assets/MyAssets/Script/ParallexBackGround.cs
+++ Assets/MyAssets/Script/ParallexBackGround.cs
@@ -8,20 +8,36 @@ public class ParallexBackGround : MonoBehaviour
 
     [SerializeField] private float parallexEffect;
     [SerializeField] private bool followY;
+
     private float xPosition;
     private float yPosition;
+
+    private float backgroundLength;
     void Start()
     {
         cam = GameObject.Find("Main Camera");
         xPosition = transform.position.x;
         yPosition = transform.position.y;
+
+
+        backgroundLength = GetComponent<SpriteRenderer>().bounds.size.x / 3;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // ±³¾°ÎÞÏÞÑÓÉê
+        float distanceMoved = cam.transform.position.x * (1 - parallexEffect);
+
+        if (distanceMoved > xPosition + backgroundLength)
+            xPosition += backgroundLength;
+        else if (distanceMoved < xPosition - backgroundLength)
+            xPosition -= backgroundLength;
+
+
+
         Vector2 distanceToMove = new Vector2(cam.transform.position.x * parallexEffect, cam.transform.position.y);
 
+        // ÊÇ·ñ¹Ì¶¨yÖá
         if (followY)
             transform.position = new Vector3(xPosition + distanceToMove.x, yPosition + distanceToMove.y);
         else
